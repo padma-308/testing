38c3bf540a6867da35fda1f369743278b772b208
padma-308 <59190585+padma-308@users.noreply.github.com>
2020-01-15 09:30:24 +0530

    Add files via upload

---
+++ RegularExpression.java
@@ -0,0 +1,35 @@
+package com.deloitte.selenium;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RegularExpression {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\chromedriver\\chromedriver.exe");
+		//DesiredCapabilities capabilities =DesiredCapabilities
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.facebook.com/campaign/landing.php?&campaign_id=973072061&extra_1=s%7Cc%7C256741341326%7Ce%7Cfacebook%7C&placement=&creative=256741341326&keyword=facebook&partner_id=googlesem&extra_2=campaignid%3D973072061%26adgroupid%3D54006406691%26matchtype%3De%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D1t1%26target%3D%26targetid%3Dkwd-297690534863%26loc_physical_ms%3D9062237%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=EAIaIQobChMIuKrDnbuC5wIVlw4rCh0NFgemEAAYASAAEgLVI_D_BwE");
+		WebElement we = driver.findElement(By.xpath("//*[@id='u_0_6']"));
+		we.sendKeys("not able to see sponsored links on google search");
+		driver.findElement(By.xpath("//*[@id='u_0_6']")).click();
+		List<WebElement> allElements = driver.findElements(By.xpath("//h3/a"));
+		for(WebElement element : allElements){
+			allElements.size();
+			System.out.println(element.getText());
+		}
+		/*String reqx = "female";
+		if(reqx.matches("driver.findElement(By xpath("/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div/div/div[1]/form/div[1]/div[6]/span/span[1]/input"))"));
+			System.out.println(true);
+		elsesss
+			System.out.println(false);
+			*/
+
+	}
+
+}
